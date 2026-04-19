440769f0cc5c743dc3e056655b2a33f833a9bd79
Jeremy De Vera <jeremydevera03@gmail.com>
2026-04-20 04:41:57 +0800

    Initial Commit

---
+++ Bacarrat Sim/Bacarrat_SimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bacarrat_SimApp.swift
+//  Bacarrat Sim
+//
+//  Created by Jeremy De Vera on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Bacarrat_SimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
